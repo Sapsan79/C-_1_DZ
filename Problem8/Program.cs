044fabc21c6d53d704b3c787596fa333ba75579a
Sapsan <semya_s@mail.ru>
2022-12-10 05:51:14 +0300

    Add Problem8

---
+++ Problem8/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая на вход принимает число (N) а на выходе выдаёт все чётные числа от 0 до N
+Console.Write("Введите число ");
+int a = int.Parse(Console.ReadLine());
+ for (int i = 2; i <= a; i += 2) {
+    Console.WriteLine (i);
+ }
