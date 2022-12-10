792f7803d8d90e09f7f107f410847f7986a5d59a
Sapsan <semya_s@mail.ru>
2022-12-10 05:15:39 +0300

    Add Problem4

---
+++ Problem4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int y = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число ");
+int z = int.Parse(Console.ReadLine());
+int max = x;
+
+if (y > max)
+{
+    max = y;
+}
+if (z > max)
+{
+    max = z;
+}
+Console.WriteLine(max);
